3363b8ab1a4e576c1aa510fbc61bb89cd745fc8d
minciong <maxraphael_inciong@yahoo.com>
2018-08-06 14:57:19 -0400

    functional data for server.js

--- DBHandler.js
+++ DBHandler.js
@@ -16,50 +16,55 @@ var registry = new ZapRegistry({networkId: 42, networkProvider: new Web3.provide
 var bondage = new ZapBondage({networkId: 42, networkProvider: new Web3.providers.WebsocketProvider(INFURA_URL)});
 
 var mysql = require('mysql');
-var pool = mysql.createPool({
-	host: DB.db,
-	user: DB.user,
-	password: DB.password,
-	database: DB.dbName 
-});
-
-var con = mysql.createConnection({ 
-	host: DB.db,
-	user: DB.user,
-	password: DB.password,
-	database: DB.dbName
-});
-
-pool.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-        		if (error) throw error;
-        		console.log('Database connection successfully established; Fetching data.');
-        	});
-        	
-// shorthand for getConnection, query, releaseConnection
-pool.query = util.promisify(pool.query);
 
 
+class DBHandler{
 
-function setProviders(providerAddress, providerTitle){
+constructor(){
+	this.pool = mysql.createPool({
+		host: DB.db,
+		user: DB.user,
+		password: DB.password,
+		database: DB.dbName 
+	});
+
+	this.con = mysql.createConnection({ 
+		host: DB.db,
+		user: DB.user,
+		password: DB.password,
+		database: DB.dbName
+	});
+
+	this.pool.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+	        		if (error) throw error;
+	        		console.log('Database connection successfully established; Fetching data.');
+	        	});
+	        	
+	// shorthand for getConnection, query, releaseConnection
+	this.pool.query = util.promisify(this.pool.query);
+
+}
+
+async setProviders(providerAddress, providerTitle){
 
 	sql = "INSERT INTO providers (provider_address, provider_title) VALUES (?,?) ON DUPLICATE KEY UPDATE provider_address = provider_address";
 	
-	pool.query(sql, [providerAddress, providerTitle],function(err, result) {
+	this.pool.query(sql, [providerAddress, providerTitle],function(err, result) {
 			if (err) throw err;
 			console.log("Inserted correctly");
 	});
 
 }
 
-function setEndpoints(provider, endptUtf, constants, parts, dividers){
+async setEndpoints(provider, endptUtf, constants, parts, dividers){
 
 	sql = "INSERT INTO endpoints (provider_address, endpoint_name, constants, parts, dividers) VALUES (?,?,?,?,?) ON DUPLICATE KEY UPDATE endpoint_name = endpoint_name";
-		await con.query(sql, [provider, endptUtf, constants, parts, dividers], function(err) {
+		await this.con.query(sql, [provider, endptUtf, constants, parts, dividers], function(err) {
 			if(err) throw(err)
 		});
 }
 
-function setBondage(endpointName,providerAddress){
+async setBondage(endpointName,providerAddress){
 	var numDots = await bondage.getDotsIssued({ provider: providerAddress, endpoint: endpointName });
 	console.log("numDots "+numDots)
 
@@ -72,15 +77,15 @@ function setBondage(endpointName,providerAddress){
 	sql3 = await "SELECT sum(zap_value) AS total_zap_value FROM endpoints WHERE provider_address=?";
 	sql4 = await "UPDATE providers SET total_zap_value=? WHERE provider_address=?";
 	
-	await pool.query(sql1, [calcZap, endpointName, providerAddress], async function(err, result) {
+	await this.pool.query(sql1, [calcZap, endpointName, providerAddress], async function(err, result) {
 		if (err) throw err;
-		await pool.query(sql2, [dotCost, endpointName, providerAddress], async function(err, result) {
+		await this.pool.query(sql2, [dotCost, endpointName, providerAddress], async function(err, result) {
 			if (err) throw err;
-			await pool.query(sql2_1, [numDots, endpointName, providerAddress], async function(err, result) {
+			await this.pool.query(sql2_1, [numDots, endpointName, providerAddress], async function(err, result) {
 			if (err) throw err;
-				await pool.query(sql3, [providerAddress], async function(err, total) {
+				await this.pool.query(sql3, [providerAddress], async function(err, total) {
 					if (err) throw err;
-					await pool.query(sql4, [total[0].total_zap_value, providerAddress], function(err, result) {
+					await this.pool.query(sql4, [total[0].total_zap_value, providerAddress], function(err, result) {
 						if (err) throw err;
 					});
 				});
@@ -92,156 +97,89 @@ function setBondage(endpointName,providerAddress){
 
 // SERVER.JS
 
-function getProviders(){
+async getProviders(){
 	var query = "SELECT * FROM providers";
-	pool.query(query, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
+	let results = await this.pool.query(query)
+	return results;
 
 }
-function getProvidersByAddress(address){
+async getProvidersByAddress(address){
 	var query = "SELECT * FROM providers WHERE provider_address=?";
-	pool.query(query, address, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-	
+	let results = await this.pool.query(query, address);
+	return results;	
 }
-function getProvidersByTitle(title){
+async getProvidersByTitle(title){
 	var query = "SELECT * FROM providers WHERE provider_title=?";
-	pool.query(query, title, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-	
+	let results = await this.pool.query(query, title)
+	return results;
 }
-function getProvidersByZap(orderBy){
+async getProvidersByZap(orderBy){
 	var query;
 	if(orderBy)
 	query = "SELECT * FROM providers WHERE total_zap_value ORDER BY total_zap_value asc";
 	else
 	query = "SELECT * FROM providers WHERE total_zap_value ORDER BY total_zap_value desc";
-
-	pool.query(query,function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
 	
+	let results = await this.pool.query(query)
+	return results;
 }
-function getProvidersLastUpdated(){
+async getProvidersLastUpdated(){
 	var query = "SELECT * FROM providers ORDER BY timestamp DESC";
-	pool.query(query, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-	
+	let results = await this.pool.query(query )
+	return results;
 }
 
 // Endpoints
 
-function getEndpoints(){
+async getEndpoints(){
 var query = "SELECT * FROM endpoints";
-	pool.query(query, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-
+	let results = await this.pool.query(query )
+	return results;
 }
-function getEndpointsByAddress(address){
+async getEndpointsByAddress(address){
 var query = "SELECT * FROM endpoints WHERE provider_address=?";
-	pool.query(query, address, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-
+	let results = await this.pool.query(query, address) 
+	return results;
 }
-function getEndpointsByName(name){
+async getEndpointsByName(name){
 var query = "SELECT * FROM endpoints WHERE endpoint_name=?";
-	pool.query(query, name, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-
+	let results = await this.pool.query(query, name) 
+	return results;
 }
-function getEndpointsByZapValue(){
+async getEndpointsByZapValue(){
 	var query;
 	if(orderBy)
 	query = "SELECT * FROM endpoints WHERE zap_value ORDER BY zap_value asc";
 	else
 	query = "SELECT * FROM endpoints WHERE zap_value ORDER BY zap_value desc";
-	pool.query(query, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-
+	let results = await this.pool.query(query) 
+	return results;
 }
-function getEndpointsByDotValue(orderBy){
+async getEndpointsByDotValue(orderBy){
 	var query;
 	if(orderBy)
 	query = "SELECT * FROM endpoints WHERE dot_value ORDER BY dot_value asc";
 	else
 	query = "SELECT * FROM endpoints WHERE dot_value ORDER BY dot_value desc";
 	
-	pool.query(query, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-
+	let results = await this.pool.query(query) 
+	return results;
 }
-function getEndpointsByDotIssued(orderBy){
+async getEndpointsByDotIssued(orderBy){
 	var query;
 	if(orderBy) 
 	query = "SELECT * FROM endpoints WHERE dot_issued ORDER BY dot_issued asc";
 	else
 	query = "SELECT * FROM endpoints WHERE dot_issued ORDER BY dot_issued desc";
-	pool.query(query, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
-
+	let results = await this.pool.query(query) 
+	return results;
 }
-function getEndpointsByTimeStamp(){
+async getEndpointsLastUpdated(){
 var query = "SELECT * FROM endpoints ORDER BY timestamp DESC";
-	pool.query(query, function(err, results) {
-		if(!err) {
-			return results;
-		}
-		else 
-			return err;
-	});
+	let results = await this.pool.query(query) 
+	return results;
+}
 
 }
+var dbHandler = new DBHandler();
+module.exports = dbHandler;
