316e4e1be504a9c3da80a92240f470bab1c3a132
Pavel <pavel.b@smartum.pro>
2018-12-18 15:29:01 +0300

    pagination impletemented and completely refactored

---
+++ js/app.js
@@ -0,0 +1,174 @@
+class OracleMarketCap {
+	constructor() {
+		this.selectRef = document.getElementById('network-select');
+		this.searchRef = document.getElementById('search-term');
+		this.paginationRef = document.getElementById('pagination');
+		this.oraclesContainerRef = document.getElementById('provider-labels').parentElement;
+		this.dialogRef = document.getElementById('dialog');
+
+		this.networks = [
+			{
+				name: 'Kovan',
+				networkProvider: 'wss://kovan.infura.io/ws',
+				networkId: 42
+			},
+			{
+				name: 'Mainnet',
+				networkProvider: 'wss://mainnet.infura.io/ws',
+				networkId: 1,
+				disabled: true
+			},
+			{
+				name: 'Localhost 8546',
+				networkProvider: 'ws://localhost:8546',
+				networkId: 1337,
+				disabled: true
+			}
+		];
+		this.providersService = null;
+		this.firstExpand = true;
+		this.pageSize = 10;
+		this.allProviderAddresses;
+		this.prevHash = {
+			expandedAddress: '',
+			page: 1,
+			search: ''
+		};
+		this.currentExpandedAddress = '';
+		this.prevRenderedOracles = [];
+
+		this.handleNetworkChange = this.handleNetworkChange.bind(this);
+		this.locationChanged = this.locationChanged.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+
+
+		initSelect(this.selectRef, this.networks, this.handleNetworkChange);
+		initFilter(this.searchRef, search => navigate({search}), parseHash().search);
+		window.addEventListener('hashchange', this.locationChanged);
+		this.oraclesContainerRef.addEventListener('click', this.handleClick);
+
+		window.addEventListener('load', async () => {
+			dialogPolyfill.registerDialog(this.dialogRef);
+			await this.handleNetworkChange(this.networks[0]);
+			this.locationChanged().catch(console.log);
+		});
+
+		this.dialogRef.addEventListener('close', () => {
+			this.closeDialog();
+		});
+		this.dialogRef.addEventListener('click', e => {
+			if (e.target.className !== 'close') return;
+			e.preventDefault();
+			this.closeDialog();
+		});
+	}
+
+	handleClick(e) {
+		const className = e.target.className;
+		switch (className) {
+			case 'fold-icon':
+				this.collapse(e);
+				break;
+			case 'copy-icon':
+				handleCopy(e);
+				break;
+		}
+	}
+
+	collapse(e) {
+		if (this.prevHash.expandedAddress.indexOf(e.target.getAttribute('data-oracle')) !== 0) return;
+		requestAnimationFrame(() => {
+			navigate({expandedAddress: ''});
+		});
+	}
+
+	handleExpandAndNavigate(address, prevAddress) {
+		const oracleAddress = address.slice(0, 42);
+		const prevOracleAddress = prevAddress.slice(0, 42);
+		if (oracleAddress !== prevOracleAddress) {
+			foldOracles(prevOracleAddress, this.prevRenderedOracles);
+		}
+		if (prevAddress) {
+			const prevHighlight = document.getElementById('_' + prevAddress) || document.getElementsByClassName(prevOracleAddress)[0];
+			if (prevHighlight) setTimeout(() => { prevHighlight.classList.remove('highlight'); }, 1500);
+		}
+		unfoldOracles(oracleAddress, this.prevRenderedOracles);
+		const element = document.getElementById('_' + address) || document.getElementsByClassName(oracleAddress)[0];
+		if (!element) return;
+		if (address.length > 42) element.classList.add('highlight');
+		if ('scrollIntoView' in element) element.scrollIntoView({
+			behavior: 'smooth',
+			block: 'center',
+			inline: 'center',
+		});
+		if (address) this.showDialog(address);
+	}
+
+	async showDialog(address) {
+		const oracleAddress = address.slice(0, 42);
+		const endpoint = address.slice(42);
+		let i = this.prevRenderedOracles.length;
+		let provider;
+		while (i--) {
+			if (this.prevRenderedOracles[i].oracle.provider.providerOwner !== oracleAddress) continue;
+			provider = this.prevRenderedOracles[i].oracle.provider;
+			break;
+		}
+		if (!provider) return;
+		const param = endpoint || 'profile';
+		const url = await getProviderParam(provider, param + '.md')
+		if (!url) return;
+		document.documentElement.classList.add('dialog-openned');
+		const container = dialog.lastElementChild;
+		container.innerHTML = `<p>Loading info ...<br>Provider: ${oracleAddress}` + (endpoint ? `<br>Endpoint: ${endpoint}</p>` : '');
+		dialog.showModal();
+		getUrlText(url).then(response => {
+			container.innerHTML = marked(response);
+		}).catch(error => {
+			const p = document.createElement('p');
+			p.textContent = error.message;
+			container.appendChild(p);
+		});
+	}
+
+	closeDialog() {
+		if (this.dialogRef.hasAttribute('open')) this.dialogRef.close();
+		document.documentElement.classList.remove('dialog-openned');
+	}
+
+	async locationChanged() {
+		this.closeDialog();
+		const hash = parseHash();
+		const { page, expandedAddress, search } = this.prevHash;
+		const oracleAddress = hash.expandedAddress.slice(0, 42);
+		if (hash.expandedAddress && expandedAddress !== hash.expandedAddress) {
+			const expandedAddressPage = getPageForItem(oracleAddress, this.allProviderAddresses, this.pageSize);
+			if (expandedAddressPage > 0) hash.page = expandedAddressPage;
+		}
+		this.prevHash = hash;
+		if (this.firstExpand || hash.page !== page || hash.search !== search) {
+			this.firstExpand = false;
+			clearOraclesRows(this.oraclesContainerRef, this.prevRenderedOracles);
+			const { items, total } = await this.getOracles(hash.page, hash.search);
+			renderPagination(this.paginationRef, getPages(hash.page, getTotalPages(total, this.pageSize)), hash);
+			this.prevRenderedOracles = render(this.oraclesContainerRef, items, hash.oracle);
+		}
+
+		this.handleExpandAndNavigate(hash.expandedAddress, expandedAddress);
+	}
+
+	async handleNetworkChange(network) {
+		clearOraclesRows(this.oraclesContainerRef, this.prevRenderedOracles);
+		this.providersService = new ProvidersService(network.networkId, network.networkProvider);
+		this.allProviderAddresses = await this.providersService.allProviderAddresses;
+	}
+
+	getOracles(page, search) {
+		const start = getPageStart(page, this.pageSize);
+		return search
+			? this.providersService.search(search, start, this.pageSize)
+			: this.providersService.getProviders(start, this.pageSize);
+	}
+}
+
+new OracleMarketCap();
---
+++ js/provideres-service.js
@@ -0,0 +1,122 @@
+class ProvidersService {
+
+  constructor(networkId, networkProvider) {
+    this.networkId = networkId;
+    this.networkProvider = networkProvider;
+    this.registry = new zapjs.ZapRegistry(this.networkOptions);
+    this.networkProvider = this.registry.provider.currentProvider;
+    this.updateProvidersData();
+    this.registry.listenNewProvider({}, () => { this.updateProvidersData(); });
+    this.registry.listenNewCurve({}, () => { this.updateProvidersData(); });
+  }
+
+  get allProviders() {
+    return this.allOraclesPromise;
+  }
+
+  get allProvidersWithTitles() {
+    return this.allProviders.then(providers => this.providersWithTitles(providers));
+  }
+
+  get allProvidersWithEndpoints() {
+    return this.allProviders.then(providers => this.providersWithEndpoints(providers));
+  }
+
+  get allProviderAddresses() {
+    return this.allProviderAddressesPromise;
+  }
+
+  get allAddresses() {
+    return this.registry.getAllProviders();
+  }
+
+  async providersWithTitles(providers) {
+    await Promise.all(providers.filter(provider => !provider.title).map((provider) => provider.getTitle()));
+    return providers;
+  }
+
+  async providersWithEndpoints(providers) {
+    await Promise.all(providers.filter(provider => !provider.endpoints).map((provider) => provider.getEndpoints()));
+    return providers;
+  }
+
+  updateProvidersData() {
+    this.allProviderAddressesPromise = this.registry.getAllProviders();
+    // do not load titles for providers on the first run, only when requested
+    this.allOraclesPromise = this.allProviderAddresses.then(providerAddresses => Promise.all(providerAddresses.map(address => this.loadProvider(address))));
+  }
+
+  loadProvider(owner) {
+    return new Provider(owner, this.networkOptions);
+  }
+
+  // TODO: For backend service return provider address and provider title only instead of provider
+  async getProviders(start = 0, limit) {
+    const providers = await this.allProviders;
+    // only get title for requested providers
+    const pageProviders = limit ? providers.slice(start, start + limit) : providers.slice(start);
+    await Promise.all([
+      this.providersWithTitles(pageProviders),
+      this.providersWithEndpoints(pageProviders),
+    ]);
+    console.log('pageProviders', pageProviders);
+    return {
+      items: pageProviders.map(provider => ({provider, endpoints: provider.endpoints})),
+      total: providers.length,
+    };
+  }
+
+  // TODO: For backend service return provider address and provider title only instead of provider
+  async search(text, start = 0, limit) {
+    if (!text.length) this.getProviders(start, limit);
+    await this.allProviders;
+    const [_, providers] = await Promise.all([
+      this.allProvidersWithEndpoints,
+      this.allProvidersWithTitles, // for search we need titles of all providers
+    ]);
+    const filteredOracles = providers.map(provider => {
+      const endpoints = provider.endpoints.filter(endpoint => endpoint && endpoint.toLowerCase().indexOf(text) !== -1);
+      if (
+        !endpoints.length
+        && provider.title.toLocaleLowerCase().indexOf(text) === -1
+        && provider.providerOwner.toLocaleLowerCase().indexOf(text) === -1
+      ) return null;
+      return {
+        provider,
+        endpoints,
+      };
+    }).filter(e => !!e);
+    return {
+      items: limit ? filteredOracles.slice(start, start + limit) : filteredOracles.slice(start),
+      total: filteredOracles.length,
+    };
+  }
+
+  get networkOptions() {
+    const handler = {
+      handleIncoming: (data) => {
+        console.log('handleIncoming', data);
+      },
+      handleUnsubscription: (data) => {
+        console.log('handleUnsubscription', data);
+      },
+      handleSubscription: (data) => {
+        console.log('handleSubscription', data);
+      },
+    };
+    return { networkId: this.networkId, networkProvider: this.networkProvider, handler };
+  }
+}
+
+class Provider extends zapjs.ZapProvider {
+	constructor(owner, options) {
+		super(owner, options);
+		this.endpoints = null;
+	}
+	async getEndpoints() {
+		if (this.endpoints) return this.endpoints;
+    const endpoints = await this.zapRegistry.getProviderEndpoints(this.providerOwner);
+    this.endpoints = endpoints;
+    return this.endpoints;
+	}
+}
